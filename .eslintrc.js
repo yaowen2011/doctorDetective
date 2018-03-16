7622d5c29b953104aa1c156f0655e59907e67b7f
guoooofu <m13224597763@163.com>
2018-03-16 16:10:23 +0800

    [A] 开启 comma-dangle 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'space-before-function-paren': 0,
-    'camelcase': 0
+    'camelcase': 0,
+    'comma-dangle': 0
   }
 }
